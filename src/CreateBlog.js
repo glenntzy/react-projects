7475c1d11a35a4fbdc81b4154baee823798f24da
glenntzy <castillonjohnglenn@gmail.com>
2022-09-16 23:39:29 +0800

    GH pages setup done

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,7 +1,49 @@
+import {useState} from 'react';
+
 const CreateBlog = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body, author};
+    
+fetch('http://localhost:8000/blogs/', {
+method: 'POST',
+headers: {"Content-Type" : "application/json"}, 
+body: JSON.stringify(blog)
+    })
+    .then(() => {
+      alert("New Blog Added");
+    })
+  }
   return (
-    <div className="create-blog">
-    <h1>Add New Blog</h1>
+    <div className="create">
+    <h2>Add New Blog</h2>
+    <form onSubmit = {handleSubmit}>
+    <label>Blog Title: </label>
+    <input
+    type="text"
+    required
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+    />
+    <label>Blog Content</label>
+    <textarea
+    required
+    value = {body}
+    onChange = {(e) => setBody(e.target.value)}
+    ></textarea>
+    <label>Author Name: </label>
+    <input
+    type = "text"
+    value = {author}
+    onChange = {(e) => setAuthor(e.target.value)}
+    />
+    <button>Add Blog</button>
+    </form>
+
     </div>
     )
 }
